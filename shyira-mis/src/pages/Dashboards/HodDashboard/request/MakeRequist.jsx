148f970b47782282549c636e21226e4aa527b28a
fidele22 <fidelehirwa2@gmail.com>
2024-08-29 12:28:47 +0200

    change code

--- shyira-mis/src/pages/Dashboards/HodDashboard/request/MakeRequist.jsx
+++ shyira-mis/src/pages/Dashboards/HodDashboard/request/MakeRequist.jsx
@@ -253,7 +253,7 @@ const LogisticRequestForm = () => {
             <label htmlFor="hodName">Name of HOD</label>
             {user ? (
               <>
-                <h1>{user.firstName} {user.lastName}</h1>
+                <h3>{user.firstName} {user.lastName}</h3>
                 <label htmlFor="hodSignature">HOD Signature:</label>
                 {user.signature ? (
                   <img src={`${process.env.REACT_APP_BACKEND_URL}/uploads/${user.signature}`} alt="Signature" />
